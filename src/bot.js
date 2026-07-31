cb7c283094e0486c8b8446d77401b3377bc4535b
stutung33-ux <stutung33@gmail.com>
2026-07-31 17:57:09 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -26,7 +26,7 @@ client.once('ready', () => {
     type: ActivityType.Listening,
     name: 'No Other Heart',
     details: 'Mac DeMarco',
-    state: 'Listening',
+    state: 'Playing',
   });
   
   console.log('✓ Status set: Listening to No Other Heart by Mac DeMarco');
