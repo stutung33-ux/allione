447ef3288c7fd5ebe46e607dd86d1aef96ef35b8
stutung33-ux <stutung33@gmail.com>
2026-07-31 17:58:44 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [ 
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "ily shadow and zeph",     // this is what people actually see
+        state: "bullshit",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
